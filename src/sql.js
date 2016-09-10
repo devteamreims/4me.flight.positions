742446a2c71661a2c4c82c0e8b6ebd14c24f3915
Benjamin Beret <benjamin.beret@gmail.com>
2016-09-10 18:24:02 +0200

    Check if SQL env vars are set, introduce SQL_TIMEOUT env var

--- src/sql.js
+++ src/sql.js
@@ -2,7 +2,6 @@ import startKnex from 'knex';
 import d from 'debug';
 const debug = d('4me.flight.positions.sql');
 
-
 let knex;
 
 export function initDb() {
@@ -13,14 +12,25 @@ export function initDb() {
     database: process.env.SQL_DB,
   };
 
+  if(!credentials.host) {
+    throw new Error('Please provided a valid SQL_HOST env variable');
+  }
+
+  if(!credentials.database) {
+    throw new Error('Please provide a valid SQL_DB env variable');
+  }
+
   debug('Connecting to DB');
+
 
   knex = startKnex({
     client: 'mysql',
     connection: {
       ...credentials,
     },
+    acquireConnectionTimeout: parseInt(process.env.SQL_TIMEOUT) || 1000,
   });
+
 
   return knex;
 }
@@ -32,5 +42,3 @@ export function getDb() {
 
   return knex;
 }
-
-export default initDb;
